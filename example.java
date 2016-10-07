f70236dc0b83bab4ad9d01638da184791780f687
Dilip pashi <dilip.pashi143@gmail.com>
2016-10-07 12:11:35 +0530

    Java_assignment_3.3
    Java_assignment_3.3

---
+++ example.java
@@ -0,0 +1,13 @@
+
+class example {
+	
+int x; int y; // Deafault value of both int variable is- 0,0
+String name; // Empty String
+
+public static void main(String[] args) {
+// new object of example class	
+example pnt = new example();
+// this will print all default values 
+System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+}
+}
